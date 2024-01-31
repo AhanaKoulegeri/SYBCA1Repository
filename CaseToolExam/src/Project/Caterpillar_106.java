632271f17488ae1dea3b41d0cd194c676fe4688e
AhanaKoulegeri <koulegeriahana@gmail.com>
2024-01-31 09:17:03 +0530

    Commit

---
+++ CaseToolExam/src/Project/Caterpillar_106.java
@@ -0,0 +1,12 @@
+package Project;
+
+public class Caterpillar_106 {
+	 int moves, cabbages;
+	 
+	 Caterpillar_106(int moves, int cabbages)
+	 {
+		 this.moves=moves;
+		 this.cabbages=cabbages;
+	 }
+
+}
