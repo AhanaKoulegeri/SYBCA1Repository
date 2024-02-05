c0cc998c90c7ba21f1328ba2ac2d0f3020deb425
Ahana Koulegeri <Ahana Koulegeri@LAPTOP-CK29C7IR>
2024-02-05 18:48:44 +0530

    Commit

---
+++ Java/src/Program/Caterpillar_106.java
@@ -0,0 +1,13 @@
+package Program;
+
+public class Caterpillar_106 {
+	int moves, cabbages;
+	 
+	 Caterpillar_106(int moves, int cabbages)
+	 {
+		 this.moves=moves;
+		 this.cabbages=cabbages;
+	 }
+
+
+}
