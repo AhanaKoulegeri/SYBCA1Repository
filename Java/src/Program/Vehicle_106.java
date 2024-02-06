4d2431ccebbfed8fcf19bde2c30856f3798580b0
Ahana Koulegeri <Ahana Koulegeri@LAPTOP-CK29C7IR>
2024-02-06 21:39:59 +0530

    Commit

---
+++ Java/src/Program/Vehicle_106.java
@@ -0,0 +1,15 @@
+package Program;
+
+public class Vehicle_106 {
+	String branddd;
+	int speed;
+	Vehicle_106(String brand)
+	{
+		this.branddd=branddd;
+		this.speed=0;
+	}
+	public static void main(String[] args) {
+		
+	}
+
+}
